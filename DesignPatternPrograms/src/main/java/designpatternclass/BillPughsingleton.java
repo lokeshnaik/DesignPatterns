c13d3dfb5a3c270eb1bab5290fb656ba717e694d
lokeshnaik <pathlavathlokeshnaik@gmail.com>
2020-02-20 15:47:31 +0530

    [LOKESH NAIK]ADD.adding Bill pugh singleton pattern implementaion

---
+++ DesignPatternPrograms/src/main/java/designpatternclass/BillPughsingleton.java
@@ -0,0 +1,37 @@
+package designpatternclass;
+/**
+ * Bill Pugh Singleton:Using instance we ijmplementing the program
+ * @author LOKESH NAIK
+ * */
+
+ class BillPughInitilization
+{
+	private  BillPughInitilization()             //private constructor is created
+	{
+		
+	}
+	public static  BillPughInitilization getInstance() // returning the object
+	{
+		return BillPughInner.INSTANCE;
+	}
+	private static class BillPughInner    //Creating inner class for creating instance
+	{
+		private static  BillPughInitilization INSTANCE=new BillPughInitilization();
+	}
+	public void display()
+	{
+		System.out.println("Just crack the coding Lokesh:");
+	}
+	
+}
+
+
+public class BillPughsingleton 
+{
+	public static void main(String[] args)
+		{
+			BillPughInitilization singleobject=BillPughInitilization.getInstance();
+	       singleobject.display();
+		}
+	
+}
